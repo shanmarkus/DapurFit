4fc9663f1e3499efdf2606686b4fcdd0aae8a71a
wiznuq <eric.wishnu@hotmail.com>
2014-01-13 14:40:02 +0700

    box in row 2 howtoorder page done

--- application/views/howtoorder.php
+++ application/views/howtoorder.php
@@ -1,167 +1,244 @@
+ <link rel="stylesheet" type="text/css" href="<?php echo RESOURCES;?>css/howtoorder.css">
  <div class="howtoorder">
-        <div class="container">
+  <div class="container">
 
-          <!-- Top Image Header Breadcrumbs-->
-          <div class="row">
-            <div class="span12">
-              <img src="<?php echo RESOURCES?>img/how-to-order_07.png">
-            </div>
+    <!-- Top Image Header Breadcrumbs-->
+    <div class="row">
+      <div class="span12 paddingbt-20">
+        <p>
+         <img src="<?php echo RESOURCES?>img/how-to-order_07.png">
+       </p>
+
+     </div>
+   </div>
+
+   <!-- We have more than 70 variants Image -->
+   <div class="row">
+    <div class="span12 paddingbt-20">
+      <div class="box">
+        <div class="row">
+          <div class="span7 text-center ft-sz-16" >
+            <p class="font-round-b">We have more than 70 variations of menu and keep adding the new ones </p></br>
+            <p class="font-round-r">Each menu are supervised by our health and nutrient expert </p>
           </div>
 
-          <!-- We have more than 70 variants Image -->
-          <div class="row">
-            <div class="span12">
-              <div class="box">
-                <div class="row">
-                  <div class="span7 text-center ft-sz-16" >
-                    <p class="font-round-b">We have more than 70 variations of menu and keep adding the new ones </p></br>
-                    <p class="font-round-r">Each menu are supervised by our health and nutrient expert </p>
-                  </div>
-             
-                  <div class="span4 text-center ft-sz-16">
-                    <p class="font-round-b ">We will costum your meal to</p></br>
-                    <p class="font-round-r">fit your taste preferences </p>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div class="span4 text-center ft-sz-16">
+            <p class="font-round-b ">We will costum your meal to</p></br>
+            <p class="font-round-r">fit your taste preferences </p>
           </div>
-
-          <!-- What Inside each dapurfit image -->
-          <div class="row">
-
-            <div class="span12">
-
-              <div class="row">
-                <div class="span12">
-                  <div class="box ft-sz-16">
-                    <div class="row">
-                      <div class="span12">
-                        <p class="font-round-b ft-sz-18"> What's Inside each <img src""/> box </p>
-                      </div>
-                      <br>
-                      <div class="span8">
-                        <div class="row">
-                          <div class="span2">
-                            <p><b>Complex Carbs</b></p>
-                            <p>The key of healthy no-starving diet</p>
-                            <p>(Red rice, Green rice, Yam, Mashed Potato, Tubber, Wheat Bread, etc)</p>
-                          </div>
-
-
-                          <div class="span2">
-                            <p><b>Lean Meat</b></p>
-                            <p>Skinless, boneless meat, very low fat, very high protein and nutrients.</p>
-                            <p>(Lean chicken's breast, Salmon, and other fishes)</p>
-                          </div>
-
-
-                          <div class="span2">
-                            <p><b>Lean Meat</b></p>
-                            <p>Skinless, boneless meat, very low fat, very high protein and nutrients.</p>
-                            <p>(Lean chicken's breast, Salmon, and other fishes)</p>
-                          </div>
-
-
-                          <div class="span2">
-                            <p><b>Lean Meat</b></p>
-                            <p>Skinless, boneless meat, very low fat, very high protein and nutrients.</p>
-                            <p>(Lean chicken's breast, Salmon, and other fishes)</p>
-                          </div>
-
-                        </div>
-                      </div>
-
-                      <div class="span2 margin-left20px">
-                        <div class="row">
-                          <div class="span2">
-                            <p><b>Complex Carbs</b></p>
-                            <p>The key of healthy no-starving diet</p>
-                            <p>(Red rice, Green rice, Yam, Mashed Potato, Tubber, Wheat Bread, etc)</p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="row paddingTop10px">
-                <div class="span11">
-                  <p class="whatInsideP">*Food is wrapped with bla bla bla bla bla bla bla bla </p>
-                </div>
-              </div>
-            </div>
-
-          </div>
-        </div>
-
-        <!-- Members Benefits and Price List -->
-        <div class="container"> 
-          <div class="row paddingTop20px margin-left20px">
-            <div class="span8 MembersBenefits">
-              <h4> Member's Benefits </h4>
-              <div class="row">
-                <div class="span1">
-                  <img src="<?php echo RESOURCES?>img/how-to-order_15.png">
-                </div>
-                <div class="span7">
-                 <h5> Workout Consultation </h5>
-                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p>
-               </div>
-             </div>
-             <div class="row paddingTop20px">
-              <div class="span1">
-                <img src="<?php echo RESOURCES?>img/how-to-order_18.png">
-              </div>
-              <div class="span7">
-                <h5> Workout Consultation </h5>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p>
-              </div>
-            </div>
-            <div class="row paddingTop20px">
-              <div class="span1">
-                <img src="<?php echo RESOURCES?>img/how-to-order_21.png">
-              </div>
-              <div class="span7">
-                <h5> Workout Consultation </h5>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p>
-              </div>
-            </div>
-          </div>
-          <!-- Price List -->
-          <div class="span3">
-            <div class="left-border paddinglr20">
-              <div class="textPriceList">
-                <h5>Pricelist (Free Delivery)</h5>
-                <p>Minimum order is 1 month subscription</p>
-                <p>First time order can try 1 week subscription</p>
-              </div>
-              <div class="HR">
-                <hr>
-              </div>
-              <div class="Meat100PriceList">
-                <h5>WITH 100gr MEAT / SERV</h5>
-                <p>2 boxes/day : 35.000 /box</p>
-                <p>1 boxex/day : 37.500 /box</p>
-              </div>
-              <div class="HR">
-                <hr>
-              </div>
-              <div class="Meat200PriceList">
-                <h5>WITH 100gr MEAT / SERV</h5>
-                <p>2 boxes/day : 45.000 /box</p>
-                <p>1 boxex/day : 47.500 /box</p>
-              </div>
-              <div class="HR">
-                <hr>
-              </div>
-            </div> <!-- close span6 -->
-          </div> <!-- close row -->
         </div>
       </div>
     </div>
+  </div>
+
+
+
+
+  <div class="row">
+    <div class="span12">
+      <div class="box">
+        <div class="row">
+          <div class="span12">
+            <p class="font-round-b ft-sz-18"> What's Inside each <img src=""/> box </p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="span12"> 
+            <table width='96%' >
+              <tr valign="top">
+                <td  width='20%'>
+                  <div>
+                    <p><div class="font-round-b ft-sz-16">Complex Carbs</div></p>
+                    <p><div class="font-normal-mi ft-sz-16">
+                      The key of healthy no-starving diet
+                    </div></br>
+                    <div class="font-normal-r ft-sz-13">
+                      (Red rice, Green rice, Yam, Mashed Potato, Tubber, Wheat Bread, etc.)</p>
+                    </div>
+                  </div>
+                </td>
+                <td  width='23%'>
+                  <div class="border-left paddinglr20">
+                   <p>
+                    <div class="font-round-b ft-sz-16">Lean Meat</div>
+                  </p>
+                  <p>
+                    <div class="font-normal-mi ft-sz-16">Skinless, boneless meat, very low fat, very high protein and nutrients.</div></br>
+                    <div class="font-normal-r ft-sz-13">(Lean chicken's breast, Salmon, and other fishes)</div>
+                  </p>
+                </div>
+              </td>
+              <td  width='20%'>
+                <div class="border-left paddinglr20">
+                 <p><div class="font-round-b ft-sz-16">Fresh Vegetables</div></p>
+                 <p>
+                   <div class="font-normal-mi ft-sz-16">For your daily fibers and vitamins.</div></br>
+                   <div class="font-normal-r ft-sz-13">(Sauteed Broccoli, Thai Salad, Capcay, Garlic Spinach, etc.)</div>
+                 </p>
+               </div>
+             </td>
+             <td  width='35%'>
+              <div class="border-left paddinglr20">
+                <p><div class="font-round-b ft-sz-16">Tasty Sambal</div></p>
+                <p>
+                  <div class="font-normal-mi ft-sz-16">
+                    Skinless, boneless meat, very low fat, very high protein and nutrients.Our sambal is cookedonly with Grapeseed Oil.
+                  </div></br>
+                  <div class="font-normal-r ft-sz-13">
+                    {a healthy essential fat that won't make you fat but help you burn fat instead}
+                  </div>
+                </p>
+              </div>
+            </td>
+                  <!-- <td width='15%'>
+                    <div class="border-left padding-left20">
+                     <p>
+                      <div class="font-round-b ft-sz-16">
+                        Eating Set
+                      </div></p>
+                      <p>
+                        <div class="font-normal-r ft-sz-13">
+                          - Spoon</br>
+                          - Fork</br>
+                          - Tissue</br>
+                          - Toothpick</br>
+                          - Plastic
+                        </div>
+                      </p>
+                    </div>
+                  </td> -->
+                </tr>
+              </table>
+            </div><!--  close span -->
+          </div><!-- close row -->
+<!--           <div class="row">
+            <div class="span3">
+              <div>
+                <p><div class="font-round-b ft-sz-16">Complex Carbs</div></p>
+                <p><div class="font-normal-mi ft-sz-16">
+                  The key of healthy no-starving diet
+                </div></br>
+                <div class="font-normal-r ft-sz-13">
+                  (Red rice, Green rice, Yam, Mashed Potato, Tubber, Wheat Bread, etc.)</p>
+                </div>
+              </div>
+            </div> 
+            <div class="span3">
+              <div class="border-left padding-left20">
+               <p>
+                <div class="font-round-b ft-sz-16">Lean Meat</div>
+              </p>
+              <p>
+                <div class="font-normal-mi ft-sz-16">Skinless, boneless meat, very low fat, very high protein and nutrients.</div></br>
+                <div class="font-normal-r ft-sz-13">(Lean chicken's breast, Salmon, and other fishes)</div>
+              </p>
+            </div>
+          </div>
+          <div class="span3">
+            <div class="border-left padding-left20">
+             <p><div class="font-round-b ft-sz-16">Fresh Vegetables</div></p>
+             <p>
+               <div class="font-normal-mi ft-sz-16">For your daily fibers and vitamins.</div></br>
+               <div class="font-normal-r ft-sz-13">(Sauteed Broccoli, Thai Salad, Capcay, Garlic Spinach, etc.)</div>
+             </p>
+           </div>
+         </div>
+         <div class="span3">
+          <div class="border-left padding-left20">
+            <p><div class="font-round-b ft-sz-16">Tasty Sambal</div></p>
+            <p>
+              <div class="font-normal-mi ft-sz-16">
+                Skinless, boneless meat, very low fat, very high protein and nutrients.Our sambal is cookedonly with Grapeseed Oil.
+              </div></br>
+              <div class="font-normal-r ft-sz-13">
+                {a healthy essential fat that won't make you fat but help you burn fat instead}
+              </div>
+            </p>
+          </div>
+        </div> 
+      </div>-->
+    </div>
 
   </div>
+</div>
+
+<div class="row paddingTop10px">
+  <div class="span12">
+    <p class="whatInsideP">*Food is wrapped with aluminium foil to preserve heat. We ensure food hygiene and on time delivery.</p>
+  </div>
+</div>
+
+</div>
+</div>
+</div>
+
+<!-- Members Benefits and Price List -->
+<div class="container"> 
+  <div class="row paddingTop20px margin-left20px">
+    <div class="span8 MembersBenefits">
+      <h4> Member's Benefits </h4>
+      <div class="row">
+        <div class="span1">
+          <img src="<?php echo RESOURCES?>img/how-to-order_15.png">
+        </div>
+        <div class="span7">
+         <h5> Workout Consultation </h5>
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p>
+       </div>
+     </div>
+     <div class="row paddingTop20px">
+      <div class="span1">
+        <img src="<?php echo RESOURCES?>img/how-to-order_18.png">
+      </div>
+      <div class="span7">
+        <h5> Workout Consultation </h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p>
+      </div>
+    </div>
+    <div class="row paddingTop20px">
+      <div class="span1">
+        <img src="<?php echo RESOURCES?>img/how-to-order_21.png">
+      </div>
+      <div class="span7">
+        <h5> Workout Consultation </h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p>
+      </div>
+    </div>
+  </div>
+  <!-- Price List -->
+  <div class="span3">
+    <div class="left-border paddinglr20">
+      <div class="textPriceList">
+        <h5>Pricelist (Free Delivery)</h5>
+        <p>Minimum order is 1 month subscription</p>
+        <p>First time order can try 1 week subscription</p>
+      </div>
+      <div class="HR">
+        <hr>
+      </div>
+      <div class="Meat100PriceList">
+        <h5>WITH 100gr MEAT / SERV</h5>
+        <p>2 boxes/day : 35.000 /box</p>
+        <p>1 boxex/day : 37.500 /box</p>
+      </div>
+      <div class="HR">
+        <hr>
+      </div>
+      <div class="Meat200PriceList">
+        <h5>WITH 100gr MEAT / SERV</h5>
+        <p>2 boxes/day : 45.000 /box</p>
+        <p>1 boxex/day : 47.500 /box</p>
+      </div>
+      <div class="HR">
+        <hr>
+      </div>
+    </div> <!-- close span6 -->
+  </div> <!-- close row -->
+</div>
+</div>
+</div>
+
+</div>
 </div><!-- /How to Order -->
 
 <div class="row paddingTop30px">

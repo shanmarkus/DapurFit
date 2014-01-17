f708b9ca644179ce8275fd6ff4a0615718d2ceb2
wiznuq <eric.wishnu@hotmail.com>
2014-01-17 17:39:26 +0700

    insert interval on carousel

--- application/views/header.php
+++ application/views/header.php
@@ -9,6 +9,8 @@
       <link href=<?php echo RESOURCES."css/home.css";?> rel="stylesheet">
       <link href=<?php echo RESOURCES."css/simpletextrotator.css";?> rel="stylesheet">
       <script src=<?php echo RESOURCES.'js/jquery-1.10.1.min.js' ?>></script>
+
+
       <script src=<?php echo BOOTS.'js/bootstrap.min.js' ?>></script>
       <title><?php echo $page_title?></title>
     </head>
--- application/views/home.php
+++ application/views/home.php
@@ -1,20 +1,26 @@
 
-      <div id="myCarousel" class="carousel slide">
-        <div class="carousel-inner">
+<div id="myCarousel" class="carousel slide">
+  <div class="carousel-inner">
 
 
-          <?php
-          for($i=0;$i<count($slideshow);$i++){?>
-          <div class="item <?php if($i==0)echo "active" ;?>">
-            <img src="<?php echo RESOURCES.$slideshow[$i];?>" alt="">
-            <div class="container">
-            </div>
-          </div>
-          <?php }?>
+    <?php
+    for($i=0;$i<count($slideshow);$i++){?>
+    <div class="item <?php if($i==0)echo "active" ;?>">
+      <img src="<?php echo RESOURCES.$slideshow[$i];?>" alt="">
+      <div class="container">
+      </div>
+    </div>
+    <?php }?>
 
-        </div>
-        <a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-        <a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>
-      </div><!-- /.carousel -->
+  </div>
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>
+</div><!-- /.carousel -->
 
+<script type="text/javascript">
+$('.carousel').carousel({
+  interval: 2000
+});
+
+</script>
 
